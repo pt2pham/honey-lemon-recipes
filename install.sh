04422d402dd75ba02333566e484c2222b47242a0
Phong Pham <phongpham1342@hotmail.com>
2020-06-23 21:45:24 -0400

    Added new deps to install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,9 @@ sudo apt update -y
 sudo apt install ruby -y 
 sudo apt install ruby-railties -y
 sudo apt install nodejs -y
+sudo apt install ruby-dev -y
+sudo apt install make -y
+sudo apt install build-essential -y
+sudo apt install libxslt-dev libxml2-dev -y
+sudo apt install libsqlite3-dev -y
 
