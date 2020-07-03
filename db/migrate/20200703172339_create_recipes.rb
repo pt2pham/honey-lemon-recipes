ed40f84ad3fbef1f45935b9eb7c61b163be4f8aa
Phong Pham <phongpham1342@hotmail.com>
2020-07-03 14:29:21 -0400

    Add Recipe create table migration

---
+++ db/migrate/20200703172339_create_recipes.rb
@@ -0,0 +1,18 @@
+class CreateRecipes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :recipes do |t|
+      t.string :name, null: false
+      t.float :difficulty, null: false
+      t.string :food_type, null: false
+      t.string :serving_size, null: false
+      t.integer :duration, null: false
+      t.text :ingredients, null: false
+      t.text :instructions, null: false
+      t.text :notes
+      t.text :tags
+      t.string :image, default: 'https://www.ilac.com/wp-content/uploads/2019/06/placeholder-600x400.png'
+
+      t.timestamps
+    end
+  end
+end
